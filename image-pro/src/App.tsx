29fc828be857a5184a77e322e3a5846b44a3c374
Pankaj Solanki <96578349+Pankajsolanki51@users.noreply.github.com>
2024-07-30 09:25:12 +0000

    done

--- image-pro/src/App.tsx
+++ image-pro/src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import Signup from "./Pages/Signup"
+import { AuthProvider } from "./Context/auth"
 
 
 function App() {
 
   return (
+    <AuthProvider>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/signup" element={<Signup/>}/>
     </Routes>
+    </AuthProvider>
   )
 }
 
--- image-pro/src/Context/auth.tsx
+++ image-pro/src/Context/auth.tsx
@@ -1,23 +1,45 @@
-import { Children, createContext, useState } from "react";
+import { FC, createContext, useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/Config";
 
-//create context
-export const AuthContext = createContext({
+// Create context
+interface AuthContextType {
+    user: User | null;
+    isLoading: boolean;
+}
+
+export const AuthContext = createContext<AuthContextType>({
     user: null,
     isLoading: false,
 });
 
-//create provider
+// Create provider
 interface AuthProviderProps {
-    children: React.ReactEventHandler
+    children: React.ReactNode;
 }
-export const AuthProvider = ({ Children }) => {
+
+export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setIsLoading(false);
+        });
+
+        return unsubscribe;
+    }, []);
+
     const value = {
         user,
-        isLoading
-    }
-    return <AuthContext.Provider value={value}>
-        {Children}
-    </AuthContext.Provider>
-}
+        isLoading,
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
